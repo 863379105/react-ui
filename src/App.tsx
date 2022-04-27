115ec93fedde4c63d51f8cb6ec517aaa23023a86
lb <863379105@qq.com>
2022-04-28 00:19:30 +0800

    init: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1>hellow world</h1>
+      <h2>hellow world</h2>
+      <h3>hellow world</h3>
+      <h4>hellow world</h4>
+      <h5>hellow world</h5>
     </div>
   );
 }
