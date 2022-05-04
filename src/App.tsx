0a9dd7bc44fa19cfbd064465d1b3e9b3c8f1c968
lb <863379105@qq.com>
2022-05-04 19:21:49 +0800

    update: Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button, {ButtonSize, ButtonType} from './components/Button/button';
+
 function App() {
   return (
     <div>
@@ -8,6 +10,17 @@ function App() {
       <h3>hellow world</h3>
       <h4>hellow world</h4>
       <h5>hellow world</h5>
+      <>
+        <Button>Default</Button>
+        <Button btnType={ButtonType.Danger} disabled>Danger</Button>
+        <Button btnType={ButtonType.Primary}>Primary</Button>
+        <Button size={ButtonSize.Large}>Large</Button>
+        <Button size={ButtonSize.Small}>Small</Button>
+        <Button size={ButtonSize.Large} btnType={ButtonType.Danger}>Large Dange</Button>
+        <Button size={ButtonSize.Small} btnType={ButtonType.Primary}>Small Primary</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">Link</Button>
+        <Button btnType={ButtonType.Link} disabled href="http://www.baidu.com">Disabled Link</Button>
+      </>
     </div>
   );
 }
