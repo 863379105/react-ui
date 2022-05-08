c1150ba2023eb90379e62626dd17fa2dced8983d
lb <863379105@qq.com>
2022-05-08 22:55:40 +0800

    add: Button test

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Button from "./button";
+
+const defaultProps = {
+  onClick: jest.fn()
+}
+
+describe('test button component', () => {
+  it('should render the correct default button',() => {
+    const wrapper = render(<Button {...defaultProps} children={'Default'} />);
+    const element = wrapper.getByText('Default');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default');
+    fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled
+  })
+})
