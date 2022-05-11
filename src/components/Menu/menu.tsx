8a2fbbf874c3321c684c4abcf8158069c9ab6e56
lb <863379105@qq.com>
2022-05-11 23:33:08 +0800

    update: Menu

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -16,6 +16,7 @@ export interface MenuProps{
 interface IMenuContext {
   index: number;
   onSelect?: SelectCallback;
+  mode?: MenuMode;
 }
 
 export const MenuContext = createContext<IMenuContext>({index:0})
@@ -48,7 +49,8 @@ const Menu: React.FC<MenuProps> = (props) => {
   }
   const passedContext: IMenuContext = {
     index: currentActive ? currentActive : 0,
-    onSelect: handleClick
+    onSelect: handleClick,
+    mode: mode,
   }
   const renderChildren = (children: React.ReactNode) => {
     return React.Children.map(children, (child, index) => {
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -17,7 +17,11 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
     className,
     children
   } = props;
+  
   const context = useContext(MenuContext);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const classes = classNames(
     'menu-item submenu-item',
     className,
@@ -25,7 +29,37 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
       'is-active': context.index === index
     }
   )
+  
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setMenuOpen(!menuOpen);
+  }
+
+  let timer: any
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setMenuOpen(toggle);
+    },300)
+  }
+
+  const clickEvents = context.mode === 'vertical' ? {
+    onClick: handleClick
+  } : {}
+  
+  const hoverEvents = context.mode !== 'vertical' ? {
+    onMouseEnter: (e: React.MouseEvent) => { handleMouse(e, true) },
+    onMouseLeave: (e: React.MouseEvent) => { handleMouse(e, false) }
+  } : {}
+
   const renderChildren = () => {
+    const classes = classNames(
+      'sub-menu',
+      {
+        'menu-opened': menuOpen
+      }
+    )
     const childrenComponent = React.Children.map(children, (child, index) => {
       const childElement = child as React.FunctionComponentElement<MenuItemProps>
       if(childElement.type.displayName === 'MenuItem') {
@@ -35,14 +69,14 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
       }
     })
     return (
-      <ul className="sub-menu">
+      <ul className={classes}>
         {childrenComponent}
       </ul>
     )
   }
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
         {title}
       </div>
       {renderChildren()}
